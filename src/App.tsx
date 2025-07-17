dd6a13b8495086c8c1bb59b86bd66c74c9a8e5c8
Diogo Constantino <diogoconstantino@amatoscar.pt>
2025-07-17 15:33:12 +0100

    SPA Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        dignissimos, nesciunt assumenda natus quia vel quidem error dolores
-        saepe officiis vitae optio aut similique ducimus? Quis esse iusto
-        dolores labore.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
