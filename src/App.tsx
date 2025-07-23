123e64cc262a0f748f1b2624bba3e28c5752ede9
Diogo Constantino <diogoconstantino@amatoscar.pt>
2025-07-23 11:30:09 +0100

    Aulas

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,7 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-
+import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              type='text'
-              labelText='Task'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <button>Enviar</button>
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
